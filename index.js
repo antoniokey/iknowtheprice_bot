8fad34328914c08898b7b772bc5a7c957855a895
Anton.Kalkou <Anton.Kalkou@itechart-group.com>
2020-12-22 12:46:52 +0300

    Added bot files

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const { Telegraf, Extra, Markup } = require('telegraf');
+const { getPriceList, preparePlacesFiles, handleLanguageAction, getBotLanguage } = require('./utils/utils');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.hears('/start', ctx => {
+  const languageQuestion = `What language do you want to use? Choose the appropriate language!`;
+  const languageOptions = [Markup.callbackButton('EN', 'en'), Markup.callbackButton('RU', 'ru')];
+
+  ctx.reply(languageQuestion, Extra.HTML().markup(Markup.inlineKeyboard(languageOptions)));
+});
+
+bot.action('en', handleLanguageAction);
+bot.action('ru', handleLanguageAction);
+
+bot.on('text', async ctx => {
+  try {
+    const places = require(`./places/places_${getBotLanguage()}.json`);
+    const incomingText = ctx.message.text;
+    const incomingPlace = incomingText[0].toUpperCase() + incomingText.slice(1);
+
+    ctx.reply(`Now the bot is getting all the price in ${incomingPlace}...`);
+
+    const url = places.find(place => place.title === incomingPlace).url;
+    const webpage = await axios.get(url);
+
+    ctx.replyWithHTML(getPriceList(webpage.data));
+  } catch(err) {
+    console.log(err.message);
+  }
+});
+
+bot
+  .launch()
+  .then(preparePlacesFiles);
---
+++ utils/utils.js
@@ -0,0 +1,127 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+let botLanguage = '';
+
+const getHeadersData = $ => {
+  try {
+    const headers = [];
+
+    $('div[id="container"] div[id="node"] h2').each((i, elem) => {
+      headers.push({ text: $(elem).text() });
+    });
+
+    headers.pop();
+
+    return headers;
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+const getListData = $ => {
+  try {
+    const list = [];
+
+    $('div[id="container"] div[id="node"] > ul').each((i, elem) => {
+      const options = [];
+
+      $(elem).children('li').map((i, elem) => options.push({ text: $(elem).text() }));
+
+      list.push(options);
+    });
+
+    return list;
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+const getPriceList = page => {
+  try {
+    const $ = cheerio.load(page);
+    const headers = getHeadersData($);
+    const list = getListData($);
+    const priceList = [];
+
+    headers.forEach((header, index) => {
+      const markdown = `
+        <b>${header.text}:</b> \n
+        ${list[index].map(option => `${option.text} \n`).join('')}
+      `;
+      
+      priceList.push(markdown);
+    });
+
+    return priceList.join('');
+  } catch(err) {
+    console.log(err);
+  }
+};
+
+const retrieveRuPlaces = async () => {
+  const places = [];
+  const webpage = await axios.get('https://www.globalprice.info');
+  const $ = cheerio.load(webpage.data);
+
+  $('#container #center #node .main_menu_column > a').each((index, item) => {
+    const title = $(item).text();
+    const url = `https:${$(item).attr('href')}`;
+
+    places.push({ title, url });
+  });
+
+  fs.writeFile(path.join(__dirname, '..', 'places', 'places_ru.json'), JSON.stringify(places), err => {
+    if (err) {
+      throw new Error(err.message);
+    }
+  });
+};
+
+const retrieveEnPlaces = async () => {
+  const places = [];
+  const webpage = await axios.get('https://www.globalprice.info/en');
+  const $ = cheerio.load(webpage.data);
+
+  $('#container #center #node .main_menu_column > a').each((index, item) => {
+    const title = $(item).text();
+    const url = `https:${$(item).attr('href')}`;
+
+    places.push({ title, url });
+  });
+
+  fs.writeFile(path.join(__dirname, '..', 'places', 'places_en.json'), JSON.stringify(places), err => {
+    if (err) {
+      throw new Error(err.message);
+    }
+
+    console.log('File was saved');
+  });
+};
+
+const preparePlacesFiles = async () => {
+  const places_en = fs.existsSync(path.join(__dirname, '..', 'places', 'places_en.json'));
+  const places_ru = fs.existsSync(path.join(__dirname, '..', 'places', 'places_ru.json'));
+
+  if (!places_en) {
+    retrieveEnPlaces();
+  }
+  if (!places_ru) {
+    retrieveRuPlaces();
+  }
+};
+
+const handleLanguageAction = ctx => {
+  const language = ctx.match;
+  const responseText = `You choose ${language.toUpperCase()} language! Now, enter the country name you want to go to in order to get price list!`;
+
+  botLanguage = language;
+
+  ctx.reply(responseText);
+};
+
+const getBotLanguage = () => botLanguage;
+
+module.exports = { getPriceList, preparePlacesFiles, handleLanguageAction, getBotLanguage };
